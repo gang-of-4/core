1c9ed4df4221c0b6fc28bbc681e09d7b90ed18d6
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2023-10-18 14:53:30 +0300

    feat: added login and sign up pages

--- apps/vendor/app/page.tsx
+++ apps/vendor/app/page.tsx
@@ -1,9 +1,17 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center gap-4 p-24">
       <h1>
         Vendor Landing Page
       </h1>
+      <Link href="/login" className="underline hover:text-blue-500">
+        Login
+      </Link>
+      <Link href="/signup" className="underline hover:text-blue-500">
+        Sign up
+      </Link>
     </main>
   )
 }
