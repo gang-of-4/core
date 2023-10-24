18c2ae1a055d97fe00789f415df6193926e4141c
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2023-10-24 19:12:51 +0300

    added metadata

--- apps/vendor/app/page.tsx
+++ apps/vendor/app/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: 'Vendor Page',
+  description: 'Vendor Landing Page',
+}
 
 export default function Home() {
   return (
@@ -6,11 +12,11 @@ export default function Home() {
       <h1>
         Vendor Landing Page
       </h1>
-      <Link href="/login" className="underline hover:text-blue-500">
+      <Link href="/auth/jwt/login" className="underline hover:text-blue-500">
         Login
       </Link>
-      <Link href="/signup" className="underline hover:text-blue-500">
-        Sign up
+      <Link href="/dashboard" className="underline hover:text-blue-500">
+        Dashboard
       </Link>
     </main>
   )
