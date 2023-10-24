d65a6afd3c184c10c5b0db051e47c85116e9154d
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2023-10-24 19:08:30 +0300

    changed login

---
+++ apps/vendor/app/auth/jwt/login/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Page from '../../../../components/login'
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Login',
+  description: 'Vendor login page'
+};
+
+
+export default function page() {
+  return (
+    <Page />
+  )
+}
