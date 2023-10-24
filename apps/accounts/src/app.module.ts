bb4753e62e44e0c1a4ff003dd08de4b7fa5e94fc
Albaraa Hasan <albaraa.hassan@zid.sa>
2023-10-24 16:08:02 +0300

    initial db connection

--- apps/accounts/src/app.module.ts
+++ apps/accounts/src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ apps/accounts/src/prisma.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { ulid } from 'ulidx';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$extends({
+      query: {
+        $allModels: {
+          async create({ args, query }) {
+            args.data = {
+              ...args.data,
+              id: ulid(),
+            };
+
+            return query(args);
+          },
+        },
+      },
+    }).$connect();
+  }
+}
