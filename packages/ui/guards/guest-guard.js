3664738e4ac2d39688f65c2fcfec30350de7a9ce
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2023-10-25 22:11:18 +0300

    commented out @todo

--- packages/ui/guards/guest-guard.js
+++ packages/ui/guards/guest-guard.js
@@ -13,9 +13,10 @@ export const GuestGuard = (props) => {
   const check = useCallback(() => {
     if (isAuthenticated) {
       // @todo make sure it works as intended
-      if (user.role === 'admin') return router.replace(`${paths.absolutePaths.admin}${paths.admin.dashboard.index}`);
-      if (user.role === 'vendor') return router.replace(`${paths.absolutePaths.vendor}${paths.vendor.dashboard.index}`);
-      router.replace(paths.absolutePaths.storefront);
+      // if (user.role === 'admin') return router.replace(`${paths.absolutePaths.admin}${paths.admin.dashboard.index}`);
+      // if (user.role === 'vendor') return router.replace(`${paths.absolutePaths.vendor}${paths.vendor.dashboard.index}`);
+      // router.replace(paths.absolutePaths.storefront);
+      router.replace('/dashboard');
     } else {
       setChecked(true);
     }
