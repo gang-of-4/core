fc30a092313500ec30e412b154e34b5f152b230a
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2023-10-25 21:59:52 +0300

    added logout for dashboard

--- apps/vendor/app/dashboard/layout.jsx
+++ apps/vendor/app/dashboard/layout.jsx
@@ -1,16 +1,30 @@
 "use client"
 
+import { useRouter } from 'next/navigation';
 import React from 'react'
 import { withAuthGuard } from 'ui/hocs/with-auth-guard'
 import { useAuth } from 'ui/hooks/use-auth'
+import { paths } from 'ui/paths';
 
-function layout() {
-    const { user } = useAuth();
+function layout({ children }) {
+  const auth = useAuth();
+  const router = useRouter();
   return (
-    <div className='w-full flex items-center justify-center p-4'>
-        Hello {user.name} 👋 
-    </div>
+    <>
+      <div className='w-full flex items-center justify-between p-4'>
+        Hello {auth.user?.name} 👋
+        <button
+          onClick={async ()=>{
+            await auth.signOut()
+            router.push(paths.vendor.index)
+          }}
+        >
+          Logout
+        </button>
+      </div>
+      {children}
+    </>
   )
 }
 
-export default withAuthGuard(layout);
+export default withAuthGuard(layout, { role: 'vendor'});
