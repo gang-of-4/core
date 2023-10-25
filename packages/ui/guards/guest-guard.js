233d8a63e110cdcb0f8008b2411af9bf74c6ef93
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2023-10-25 21:58:37 +0300

    route based on role

--- packages/ui/guards/guest-guard.js
+++ packages/ui/guards/guest-guard.js
@@ -2,18 +2,20 @@ import { useCallback, useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import PropTypes from 'prop-types';
 import { useAuth } from '../hooks/use-auth';
-// import { paths } from '../paths';
+import { paths } from '../paths';
 
 export const GuestGuard = (props) => {
   const { children } = props;
-  const { isAuthenticated } = useAuth();
+  const { user, isAuthenticated } = useAuth();
   const router = useRouter();
   const [checked, setChecked] = useState(false);
 
   const check = useCallback(() => {
     if (isAuthenticated) {
-      // router.replace(paths.dashboard.index);
-      router.replace('/dashboard');
+      // @todo make sure it works as intended
+      if (user.role === 'admin') return router.replace(`${paths.absolutePaths.admin}${paths.admin.dashboard.index}`);
+      if (user.role === 'vendor') return router.replace(`${paths.absolutePaths.vendor}${paths.vendor.dashboard.index}`);
+      router.replace(paths.absolutePaths.storefront);
     } else {
       setChecked(true);
     }
