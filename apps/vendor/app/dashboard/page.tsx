a9ab321d385dad8cfbd975dd7b8f1790a46c361e
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2023-10-25 22:11:02 +0300

    modified style

--- apps/vendor/app/dashboard/page.tsx
+++ apps/vendor/app/dashboard/page.tsx
@@ -2,12 +2,16 @@ import { Metadata } from 'next'
 import React from 'react'
 
 export const metadata: Metadata = {
-    title: 'Vendor Dashboard',
-    description: 'Vendor Dashboard',
+  title: 'Vendor Dashboard',
+  description: 'Vendor Dashboard',
 }
 
 export default function page() {
   return (
-    <div>Vendor Dashboard</div>
+    <>
+      <h1 className='w-full h-screen flex justify-center items-center'>
+        Vendor Dashboard will be added here 🚧 🕙
+      </h1>
+    </>
   )
 }
