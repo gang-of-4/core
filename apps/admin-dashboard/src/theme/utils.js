d5d4364ec0ed147ace9b6c71748945950b751867
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2023-10-28 14:24:14 +0300

    removed unneeded themes

--- apps/admin-dashboard/src/theme/utils.js
+++ apps/admin-dashboard/src/theme/utils.js
@@ -4,12 +4,6 @@ export const getPrimary = (preset) => {
   switch (preset) {
     case 'blue':
       return blue;
-    case 'green':
-      return green;
-    case 'indigo':
-      return indigo;
-    case 'purple':
-      return purple;
     default:
       console.error('Invalid color preset, accepted values: "blue", "green", "indigo" or "purple"".');
       return blue;
