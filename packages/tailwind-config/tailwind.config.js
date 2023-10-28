6cf868ff57c664f07389c9908b55802404edcab5
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2023-10-28 09:04:55 +0300

    added tailwind theme

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -9,17 +9,38 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                "special-blue": {
-                    DEFAULT: "#d3d3d3",
-                }
-            },
-            fontSize: {
-                "2xs": "8px",
-            },
-            backgroundImage: {
-                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-                'gradient-conic':
-                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+                "primary": {
+                    DEFAULT: "#2970FF",
+                    50: "#F5F8FF",
+                    100: "EBEFFF",
+                    200: "#2970FF",
+                    300: "#004EEB",
+                    400: "#00359E",
+                },
+                "secondary": {
+                    50: '#F8F9FA',
+                    100: '#F3F4F6',
+                    200: '#E5E7EB',
+                    300: '#D2D6DB',
+                    400: '#9DA4AE',
+                    500: '#6C737F',
+                    600: '#4D5761',
+                    700: '#2F3746',
+                    800: '#1C2536',
+                    900: '#111927',
+                },
+                "success": {
+                    DEFAULT: "#10B981",
+                },
+                "warning": {
+                    DEFAULT: "#F79009",
+                },
+                "error": {
+                    DEFAULT: "#F04438",
+                },
+                "info": {
+                    DEFAULT: "#06AED4",
+                },
             },
         },
     },
