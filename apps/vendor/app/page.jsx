7bf40b07eaf39c41d6f01fea9ecfe0acfd22d180
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2023-10-28 14:24:45 +0300

    removed links

--- apps/vendor/app/page.jsx
+++ apps/vendor/app/page.jsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-import { paths } from "ui/paths";
 
 export const metadata = {
   title: 'Vendor Page',
@@ -8,16 +6,12 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-4 p-24">
-      <h1>
-        Vendor Landing Page
-      </h1>
-      <Link href={paths.auth.login} className="underline hover:text-blue-500">
-        Login
-      </Link>
-      <Link href={paths.vendor.dashboard.index} className="underline hover:text-blue-500">
-        Dashboard
-      </Link>
-    </main>
+    <>
+      <main className="flex min-h-screen flex-col items-center gap-4 p-24">
+        <h1>
+          Vendor Landing Page
+        </h1>
+      </main>
+    </>
   )
 }
