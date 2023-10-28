28732d49bbab94226bbc9bb28c4077d7e5f9c3a9
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2023-10-28 14:25:56 +0300

    add mui theme provider

--- apps/vendor/components/Providers.jsx
+++ apps/vendor/components/Providers.jsx
@@ -4,13 +4,30 @@ import React from 'react';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { AuthProvider } from 'ui/contexts/auth/jwt-context';
+import { createTheme } from 'ui/theme'
+import { ThemeProvider } from '@mui/material/styles';
+
 
 export function Providers({ children }) {
+
+  const theme = createTheme({
+      colorPreset: 'blue',
+      contrast: 'normal',
+      direction: 'ltr',
+      layout: 'vertical',
+      navColor: 'evident',
+      paletteMode: 'light',
+      responsiveFontSizes: true,
+      stretch: false
+    })
+
   return (
     <>
       <LocalizationProvider dateAdapter={AdapterDateFns}>
         <AuthProvider>
+          <ThemeProvider theme={theme}>
             {children}
+          </ThemeProvider>
         </AuthProvider>
       </LocalizationProvider>
     </>
