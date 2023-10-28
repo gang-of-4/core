2df4343a59adcb3b9dd317d0e4f82e63b64ae977
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2023-10-28 16:53:54 +0300

    changed file names to app router conventions

---
+++ apps/storefront/app/error.js
@@ -0,0 +1,85 @@
+import NextLink from 'next/link';
+import Head from 'next/head';
+import { Box, Button, Container, Typography, useMediaQuery } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import { paths } from 'ui/paths';
+import { organization } from 'ui/config/index.js';
+
+const Page = () => {
+  const theme = useTheme();
+  const mdUp = useMediaQuery(theme.breakpoints.up('md'));
+  const name = organization.name; 
+
+
+  usePageView();
+
+  return (
+    <>
+      <Head>
+        <title>
+          Error: Server Error | {name}
+        </title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          alignItems: 'center',
+          display: 'flex',
+          flexGrow: 1,
+          py: '80px'
+        }}
+      >
+        <Container maxWidth="lg">
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              mb: 6
+            }}
+          >
+            <Box
+              alt="Internal server error"
+              component="img"
+              src="/assets/errors/error-500.png"
+              sx={{
+                height: 'auto',
+                maxWidth: '100%',
+                width: 400
+              }}
+            />
+          </Box>
+          <Typography
+            align="center"
+            variant={mdUp ? 'h1' : 'h4'}
+          >
+            500: Internal Server Error
+          </Typography>
+          <Typography
+            align="center"
+            color="text.secondary"
+            sx={{ mt: 0.5 }}
+          >
+            You either tried some shady route or you came here by mistake. Whichever it is, try using the navigation.
+          </Typography>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              mt: 6
+            }}
+          >
+            <Button
+              component={NextLink}
+              href={paths.storefront.index}
+            >
+              Back to Home
+            </Button>
+          </Box>
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+export default Page;
+
