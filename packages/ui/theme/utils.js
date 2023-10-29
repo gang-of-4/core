7e17b793af1dd78ecdcf9737758bd00950b093a6
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2023-10-29 14:56:16 +0300

    removed error and other themes

--- packages/ui/theme/utils.js
+++ packages/ui/theme/utils.js
@@ -1,17 +1,5 @@
-import { blue, green, indigo, purple } from './colors';
+import { blue } from './colors';
 
 export const getPrimary = (preset) => {
-  switch (preset) {
-    case 'blue':
-      return blue;
-    case 'green':
-      return green;
-    case 'indigo':
-      return indigo;
-    case 'purple':
-      return purple;
-    default:
-      console.error('Invalid color preset, accepted values: "blue", "green", "indigo" or "purple"".');
-      return blue;
-  }
+  return blue;
 };
