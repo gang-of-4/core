06d75adc0b99000df389f75e7f6f4601bd804705
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2023-11-13 06:56:21 +0300

    fix header

--- apps/vendor/app/(home)/layout.jsx
+++ apps/vendor/app/(home)/layout.jsx
@@ -4,8 +4,9 @@ import React from 'react'
 function layout({ children }) {
   return (
     <>
-      <Header />
-      {children}
+      <Header>
+        {children}
+      </Header>
     </>
   )
 }
