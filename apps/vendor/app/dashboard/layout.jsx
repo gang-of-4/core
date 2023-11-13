ce589b3c2cb41d455371229b70bf1cd4705852f7
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2023-11-13 16:23:34 +0300

    added paths and redirecting

--- apps/vendor/app/dashboard/layout.jsx
+++ apps/vendor/app/dashboard/layout.jsx
@@ -2,13 +2,14 @@
 
 import { useStores } from '@/hooks/useStores'
 import { useParams, useRouter } from 'next/navigation'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { withAuthGuard } from 'ui/hocs/with-auth-guard'
 import { Layout as DashboardLayout } from 'ui/layouts/dashboard'
 import { Button, SvgIcon } from '@mui/material';
 import PlusIcon from '@untitled-ui/icons-react/build/esm/Plus';
 import NextLink from 'next/link';
 import { getSections } from '@/components/dashboard/Sections'
+import { paths } from 'ui/paths'
 
 
 function layout({ children }) {
@@ -20,11 +21,10 @@ function layout({ children }) {
   
   const optionsList = stores?.map(store => store.name);
 
-  // const [currentStore, setCurrentStore] = useState();
 
   function handleChange(storeName) {
     const store = stores.find(store => store.name === storeName);
-    router.push(`/dashboard/stores/${store.id}`)
+    router.push(`${paths.vendor.dashboard.stores.index}/${store.id}`)
   }
 
   let currentStore = {};
@@ -33,12 +33,19 @@ function layout({ children }) {
     currentStore = stores?.find(store => store.id === params.id[0]);
   }
 
+  useEffect(() => {
+    if (stores?.length === 0) {
+      console.log('No stores found for this vendor, redirecting to onboarding')
+      router.push(paths.vendor.onboarding.index)
+    }
+  }, [])
+
 
   const createStore = (
     <>
       <Button
         component={NextLink}
-        href={'/'}
+        href={`/${paths.vendor.dashboard.stores.create}`}
         variant="contained"
         endIcon={(
           <SvgIcon>
