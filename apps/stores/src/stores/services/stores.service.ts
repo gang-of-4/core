30d9d6dbbf6b61a71276eb24cc323edc39427293
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2023-11-21 11:04:38 +0300

    added service

---
+++ apps/stores/src/stores/services/stores.service.ts
@@ -0,0 +1,161 @@
+import { Injectable } from '@nestjs/common';
+import { CreateStoreDto } from '../dto/create-store.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateBusinessStoreDto } from '../dto/create-business-store.dto';
+import { UpdateBusinessStoreDto } from '../dto/update-business-store.dto';
+import { StoreEntity } from '../entities/store.entity';
+import { Status, Store, User } from '@prisma/client';
+import { UpdateStoreDto } from '../dto/update-store-dto';
+
+@Injectable()
+export class StoresService {
+  constructor(
+    private prisma: PrismaService,
+  ) { }
+
+  async createIndividual(createStoreDto: CreateStoreDto): Promise<StoreEntity> {
+
+    const stores = await this.findByVendor(createStoreDto.vendorId);
+
+    console.log('stores service | stores: ', stores);
+
+    stores.forEach(async (store) => {
+      const individualStore = await this.prisma.individualStore.findUnique({
+        where: {
+          storeId: store.id,
+        },
+      });
+      if (individualStore) {
+        throw new Error('Vendor ID already has an individual store');
+      }
+    });
+
+    return new StoreEntity(
+      await this.prisma.store.create({
+        data: {
+          vendorId: createStoreDto.vendorId,
+          status: createStoreDto.status || Status.PENDING,
+          individualStore: {
+            create: {}
+          }
+        }
+      })
+    );
+  }
+
+  async createBusiness(createBusinessStoreDto: CreateBusinessStoreDto) {
+    return new StoreEntity(
+      await this.prisma.store.create({
+        data: {
+          vendorId: createBusinessStoreDto.vendorId,
+          status: Status.PENDING,
+          businessStore: {
+            create: {
+              name: createBusinessStoreDto.name,
+              logo: createBusinessStoreDto.logo,
+              vat_number: createBusinessStoreDto.vatNumber,
+              cr_number: createBusinessStoreDto.crNumber,
+              owner_national_id: createBusinessStoreDto.ownerNationalId,
+            }
+          }
+        }
+      })
+    );
+  }
+
+  async findAll(): Promise<StoreEntity[]> {
+    const stores = await this.prisma.store.findMany(
+      {
+        where: {
+          deleted_at: null,
+        },
+        include: {
+          individualStore: true,
+          businessStore: true,
+        },
+      }
+    );
+    return stores.map((store) => new StoreEntity(store));
+  }
+
+  async findOne(id: Store['id']): Promise<StoreEntity> {
+    return new StoreEntity(
+      await this.prisma.store.findUnique({
+        where: {
+          id,
+          deleted_at: null,
+        },
+        include: {
+          individualStore: true,
+          businessStore: true,
+        },
+      }),
+    );
+  };
+
+  async findByVendor(id: User['id']): Promise<StoreEntity[]> {
+    const stores = await this.prisma.store.findMany({
+      where: {
+        vendorId: id,
+        deleted_at: null,
+      },
+      include: {
+        individualStore: true,
+        businessStore: true,
+      },
+    });
+    return stores.map((store) => new StoreEntity(store));
+  }
+
+  async update(id: Store['id'], updateStoreDto: UpdateStoreDto): Promise<StoreEntity> {
+    return new StoreEntity(
+      await this.prisma.store.update({
+        where: {
+          id,
+        },
+        data: {
+          status: updateStoreDto.status,
+        },
+      }),
+    );
+  }
+
+  async updateBusiness(id: Store['id'], updateBusinessStoreDto: UpdateBusinessStoreDto) {
+    return new StoreEntity(
+      await this.prisma.store.update({
+        where: {
+          id,
+          deleted_at: null,
+        },
+        data: {
+          businessStore: {
+            update: {
+              name: updateBusinessStoreDto.name,
+              logo: updateBusinessStoreDto.logo,
+              vat_number: updateBusinessStoreDto.vatNumber,
+              cr_number: updateBusinessStoreDto.crNumber,
+              owner_national_id: updateBusinessStoreDto.ownerNationalId,
+            }
+          }
+        },
+        include: {
+          businessStore: true,
+        },
+      })
+    );
+  }
+
+  async remove(id: Store['id']) {
+    return new StoreEntity(
+      await this.prisma.store.update({
+        where: {
+          id,
+        },
+        data: {
+          deleted_at: new Date(),
+        },
+      })
+    );
+  }
+
+}
