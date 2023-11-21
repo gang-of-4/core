e5f27888949a090c03ca1b00644745358f39cf7b
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2023-11-21 11:06:58 +0300

    modify app module

--- apps/stores/src/app.module.ts
+++ apps/stores/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { StoresModule } from './stores/stores.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    StoresModule, 
+    PrismaModule, 
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
