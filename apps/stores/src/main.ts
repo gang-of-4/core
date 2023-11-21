420719a5c07b6ca85e10eb76b92e0477cbefae70
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2023-11-21 11:07:09 +0300

    modify main

--- apps/stores/src/main.ts
+++ apps/stores/src/main.ts
@@ -1,8 +1,28 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  })
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const config = new DocumentBuilder()
+    .setTitle('Stores Service')
+    .setDescription('Manage vendor stores')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs/stores', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
