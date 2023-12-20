06846a4f8c6930e492ce14a7192acda71d9178e5
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2023-12-20 17:00:55 +0300

    modified admin layouts

--- apps/admin-dashboard/src/layouts/dashboard/index.js
+++ apps/admin-dashboard/src/layouts/dashboard/index.js
@@ -2,7 +2,6 @@ import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 import { useSettings } from '../../hooks/use-settings';
-import { HorizontalLayout } from './horizontal-layout';
 import { VerticalLayout } from './vertical-layout';
 import { getSections } from './config';
 import { withAuthGuard } from '../../hocs/with-auth-guard';
@@ -16,15 +15,6 @@ const useTranslatedSections = () => {
 export const Layout = withAuthGuard((props) => {
   const settings = useSettings();
   const sections = useTranslatedSections();
-
-  if (settings.layout === 'horizontal') {
-    return (
-      <HorizontalLayout
-        sections={sections}
-        navColor={settings.navColor}
-        {...props} />
-    );
-  }
 
   return (
     <VerticalLayout
