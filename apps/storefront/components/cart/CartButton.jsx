0a9f982f56daa5d64f1e0fe7a7a34c15dd164ea7
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2024-03-07 08:38:58 +0300

    modified cart button

--- apps/storefront/components/cart/CartButton.jsx
+++ apps/storefront/components/cart/CartButton.jsx
@@ -1,15 +1,25 @@
+"use client"
 import ShoppingCart01Icon from '@untitled-ui/icons-react/build/esm/ShoppingCart01';
 import { Badge, IconButton, SvgIcon, Tooltip } from '@mui/material';
+import { useCart } from '@/contexts/CartContext';
+import NextLink from 'next/link';
 
 
 export function CartButton() {
 
+  const { cartItems } = useCart();
+
+
   return (
     <>
       <Tooltip title="Cart">
-        <IconButton>
+        <IconButton
+          component={NextLink}
+          href="/cart"
+        >
           <Badge
-            color="error"
+            color="primary"
+            badgeContent={cartItems.length}
           >
             <SvgIcon>
               <ShoppingCart01Icon />
