63788fb25d399fd21156d5e6285534885de110e3
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2024-03-07 08:39:15 +0300

    add AuthGuard to page

--- apps/storefront/app/cart/page.jsx
+++ apps/storefront/app/cart/page.jsx
@@ -1,9 +1,12 @@
 
+import AuthGuard from '@/components/auth/auth-guard'
 import CartPage from '@/components/cart/CartPage'
 import React from 'react'
 
 export default function page() {
   return (
-    <CartPage />
+    <AuthGuard role='customer'>
+      <CartPage />
+    </AuthGuard>
   )
 }
