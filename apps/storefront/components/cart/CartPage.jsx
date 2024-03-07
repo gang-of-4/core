efad25492a62da7a1002df8112c9c37dfa756bc3
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2024-03-07 08:39:41 +0300

    used context in cart page

--- apps/storefront/components/cart/CartPage.jsx
+++ apps/storefront/components/cart/CartPage.jsx
@@ -1,8 +1,12 @@
 "use client"
+import { useCart } from '@/contexts/CartContext';
 import { Box, Container } from '@mui/material'
 import React from 'react'
 
 export default function CartPage() {
+
+    const { cartItems } = useCart();
+
     return (
         <Box
             component="main"
@@ -12,6 +16,10 @@ export default function CartPage() {
         >
             <Container maxWidth={'lg'}>
                 Cart Page
+
+                <Box>
+                    {JSON.stringify(cartItems, null, 2)}
+                </Box>
             </Container>
         </Box>
     )
