6b338c7f8e415af9f0f07fbf139011b72299334a
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2024-03-08 10:28:33 +0300

    added cart page

--- apps/storefront/components/cart/CartPage.jsx
+++ apps/storefront/components/cart/CartPage.jsx
@@ -1,26 +1,92 @@
 "use client"
 import { useCart } from '@/contexts/CartContext';
-import { Box, Container } from '@mui/material'
+import { Box, Button, Card, CardHeader, Container, Divider, Grid, IconButton, List, ListItem, ListItemAvatar, ListItemButton, ListItemSecondaryAction, ListItemText, OutlinedInput, Stack, SvgIcon, Tooltip, Typography } from '@mui/material'
+import Image01Icon from '@untitled-ui/icons-react/build/esm/Image01';
+import Trash01Icon from '@untitled-ui/icons-react/build/esm/Trash01';
+import ArrowLeftIcon from '@untitled-ui/icons-react/build/esm/ArrowLeft';
 import React from 'react'
+import NextLink from 'next/link'
+import CartActions from './CartActions';
+import CartItem from './CartItem';
+
 
 export default function CartPage() {
 
-    const { cartItems } = useCart();
+  const { cartItems, removeFromCart } = useCart();
 
-    return (
-        <Box
-            component="main"
-            sx={{
-                flexGrow: 1,
-            }}
+  async function handleRemoveItem(itemId) {
+    await removeFromCart(itemId);
+  }
+
+  return (
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+      }}
+    >
+      <Container maxWidth={'lg'}>
+        <Grid
+          container
+          spacing={{
+            xs: 3,
+            lg: 4
+          }}
         >
-            <Container maxWidth={'lg'}>
-                Cart Page
 
-                <Box>
-                    {JSON.stringify(cartItems, null, 2)}
-                </Box>
-            </Container>
-        </Box>
-    )
+          <Grid item xs={12}>
+            <Stack spacing={3}>
+              <Box
+                component={NextLink}
+                href={`/catalog/items`}
+                color="primary.main"
+
+                sx={{
+                  alignItems: 'center',
+                  display: 'flex',
+                  justifyContent: 'flex-start',
+                  px: 2,
+                  pt: 4,
+                  ":hover": {
+                    textDecoration: "underline",
+                  }
+                }}>
+                <SvgIcon sx={{ mr: 1 }}>
+                  <ArrowLeftIcon />
+                </SvgIcon>
+                <Typography variant="subtitle2">
+                  Back to Catalog
+                </Typography>
+              </Box>
+            </Stack>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Card
+              variant="outlined"
+              sx={{ p: 3 }}
+            >
+              <CardHeader title="Your Cart" />
+
+              <List>
+                {cartItems.map((cartItem) => (
+                  <CartItem
+                    key={cartItem.id}
+                    cartItem={cartItem}
+                    handleRemoveItem={handleRemoveItem}
+                  />
+                ))}
+              </List>
+
+              <Divider />
+
+              <CartActions />
+
+            </Card>
+          </Grid>
+
+        </Grid>
+      </Container>
+    </Box >
+  )
 }
