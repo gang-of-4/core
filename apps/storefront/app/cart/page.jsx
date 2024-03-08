611735491ddc70ccbb698c8b23e93efefd3ed665
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2024-03-08 10:28:12 +0300

    added cart page metadata

--- apps/storefront/app/cart/page.jsx
+++ apps/storefront/app/cart/page.jsx
@@ -3,6 +3,11 @@ import AuthGuard from '@/components/auth/auth-guard'
 import CartPage from '@/components/cart/CartPage'
 import React from 'react'
 
+export const meta = {
+  title: 'Cart',
+  description: 'Your cart',
+}
+
 export default function page() {
   return (
     <AuthGuard role='customer'>
