d479b5108073996241692a73d6c88b787874415a
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2024-03-23 02:40:58 +0300

    added get media

--- apps/media/src/media/media.module.ts
+++ apps/media/src/media/media.module.ts
@@ -3,10 +3,11 @@ import { MediaService } from './services/media.service';
 import { UploadController } from './controllers/upload.controller';
 import { AwsModule } from 'src/aws/aws.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { MediaController } from './controllers/media.controller';
 
 @Module({
   imports: [AwsModule, PrismaModule],
-  controllers: [UploadController],
+  controllers: [UploadController, MediaController],
   providers: [MediaService],
 })
 export class MediaModule {}
