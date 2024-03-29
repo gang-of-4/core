136a02fdc1aefeaada7fd332281da3c303e945ff
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2024-03-29 03:02:35 +0300

    init app module

--- apps/cart/src/app.module.ts
+++ apps/cart/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CartModule } from './cart/cart.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    CartModule,
+    PrismaModule,
+    JwtModule.register({
+      global: true,
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
