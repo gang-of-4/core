d81f589e945b78cbeda41bd716a18b2982e29385
Albaraa Hasan <albaraa.hassan@zid.sa>
2024-04-16 06:13:47 +0300

    move to controllers folder

--- apps/cart/src/cart/cart.module.ts
+++ apps/cart/src/cart/cart.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { CartService } from './services/cart.service';
-import { CartController } from './cart.controller';
+import { CartController } from './controllers/cart.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ItemsService } from './services/items.service';
