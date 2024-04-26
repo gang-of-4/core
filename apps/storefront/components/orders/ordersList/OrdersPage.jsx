845d9a4fad9eb0342a317d2cf3fb497286b2619d
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2024-04-26 23:42:13 +0300

    added orders page

---
+++ apps/storefront/components/orders/ordersList/OrdersPage.jsx
@@ -0,0 +1,117 @@
+"use client";
+import { useAuth } from "@/contexts/AuthContext";
+import fetchApi from "@/utils/fetch-api";
+import {
+  Box,
+  Card,
+  Container,
+  Divider,
+  Grid,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { useCallback, useEffect, useState } from "react";
+import { OrdersListSearch } from "./OrdersListSearch";
+import { OrdersListTable } from "./OrdersListTable";
+import { capitalize } from "@/utils/format-string";
+import { config } from "ui/config";
+
+const useSearch = () => {
+  const [search, setSearch] = useState({
+    filters: {
+      name: undefined,
+      status: [],
+    },
+  });
+
+  return {
+    search,
+    updateSearch: setSearch,
+  };
+};
+
+export default function OrdersPage() {
+  const [orders, setOrders] = useState([]);
+  const [error, setError] = useState(null);
+  const { user, isInitialized } = useAuth();
+  const { search, updateSearch } = useSearch();
+
+  useEffect(() => {
+    const interval = setTimeout(() => {
+      if (isInitialized && user?.id) {
+        fetchOrders(search);
+      }
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, [isInitialized, user, search]);
+
+  async function fetchOrders(search) {
+    const filters = {
+      ...search.filters,
+    };
+
+    try {
+      const { data } = await fetchApi({
+        url: `/api/orders?q=${filters.name}&status=${filters.status}&sort=${filters.sort}`,
+      });
+      setOrders(data);
+      setError(null);
+    } catch (error) {
+      console.error(error);
+      setError(error.message);
+    }
+  }
+
+  const handleFiltersChange = useCallback(
+    (filters) => {
+      updateSearch((prevState) => ({
+        ...prevState,
+        filters: {
+          ...prevState.filters,
+          ...filters,
+        },
+      }));
+    },
+    [updateSearch]
+  );
+
+  return (
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+      }}
+    >
+      <Container maxWidth={"lg"}>
+        <Grid
+          container
+          spacing={{
+            xs: 3,
+            lg: 4,
+          }}
+        >
+          <Grid item xs={12}>
+            <Stack direction="row" justifyContent="space-between" spacing={4}>
+              <Typography variant="h4">My {capitalize(config.order.plural)}</Typography>
+            </Stack>
+          </Grid>
+          <Grid item xs={12}>
+            <Card>
+              <Stack>
+                <OrdersListSearch onFiltersChange={handleFiltersChange} />
+              </Stack>
+              <Divider />
+              <Stack>
+                {orders && orders.length > 0 && (
+                  <OrdersListTable orders={orders} />
+                )}
+                {error && <Typography color="error">{error}</Typography>}
+              </Stack>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+}
---
+++ apps/storefront/utils/format-order.js
@@ -0,0 +1,38 @@
+export async function formatOrder({
+  order,
+  includeUser = false,
+  includeStore = false,
+  token,
+}) {
+  if (includeUser) {
+    const res = await fetch(`${process.env.USERS_API_URL}/${order.userId}`, {
+      headers: {
+        Authorization: token,
+      },
+    });
+    const data = await res.json();
+    order.user = data;
+  }
+  if (includeStore) {
+    order.orderItems = await Promise.all(
+      order.orderItems.map(async (orderItem) => {
+        const res = await fetch(
+          `${process.env.STORES_API_URL}/${orderItem.storeId}`,
+          {
+            headers: {
+              Authorization: token,
+            },
+          }
+        );
+        const data = await res.json();
+        console.log(data);
+        return {
+          ...orderItem,
+          store: data,
+        };
+      })
+    );
+  }
+
+  return order;
+}
