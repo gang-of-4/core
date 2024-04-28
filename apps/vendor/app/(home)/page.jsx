84ced5de6c8599d2dd55343846de3409bfb12ad8
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2024-04-28 20:14:15 +0300

    added vendor landing page

--- apps/vendor/app/(home)/page.jsx
+++ apps/vendor/app/(home)/page.jsx
@@ -1,30 +1,15 @@
-import Link from "next/link"
+import LandingPage from "@/components/home/LandingPage"
+import { config } from "ui/config"
 
 export const metadata = {
-  title: 'Vendor Page',
-  description: 'Vendor Landing Page',
+  title: `${config.platformName} | Vendor`,
+
 }
 
 export default function Home() {
   return (
     <>
-      <main className="flex min-h-full flex-col items-center gap-4">
-        <h1>
-          Welcome to the Vendor Landing Page
-        </h1>
-        <Link href="/dashboard" className="
-          bg-primary
-          hover:bg-primary-400
-          text-white
-          font-bold
-          py-2
-          px-4
-          rounded-xl
-          transition
-        ">
-          Go to Vendor Dashboard
-        </Link>
-      </main>
+      <LandingPage />
     </>
   )
 }
