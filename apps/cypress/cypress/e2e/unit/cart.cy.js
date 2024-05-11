00c50909f0963a248aeceb3d545f748051b3d29c
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2024-05-11 22:53:35 +0300

    moved token to imports

--- apps/cypress/cypress/e2e/unit/cart.cy.js
+++ apps/cypress/cypress/e2e/unit/cart.cy.js
@@ -26,8 +26,7 @@ describe("Add to Cart", () => {
     cy.intercept("POST", "/api/auth/login*", {
       statusCode: 200,
       body: {
-        accessToken:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI5MTQwNGMzOC1kZjA4LTQ3YWEtYjEyYS00MTVjYjc3NTE2ZTgiLCJ1c2VyIjp7ImlkIjoiOTE0MDRjMzgtZGYwOC00N2FhLWIxMmEtNDE1Y2I3NzUxNmU4IiwiZmlyc3ROYW1lIjoiSmFzb24iLCJsYXN0TmFtZSI6IkdhcnJldHQiLCJlbWFpbCI6ImRpYnlAbWFpbGluYXRvci5jb20iLCJwaG9uZSI6bnVsbCwiZW1haWxWZXJpZmllZEF0IjpudWxsLCJjcmVhdGVkQXQiOiIyMDI0LTA1LTA1VDE3OjEzOjE0LjI5NVoiLCJ1cGRhdGVkQXQiOiIyMDI0LTA1LTA1VDE3OjEzOjE0LjI5NVoiLCJyb2xlIjp7ImlkIjoiMGUzNzQ0NjgtNzlmMS00MWU5LTlkOTEtMmNjMTczNTZhNTI5IiwibmFtZSI6ImN1c3RvbWVyIn19LCJpYXQiOjE3MTQ5MjkxOTUsImV4cCI6MTcxNTEwMTk5NX0.-JrzQQLk3CpEwPV-RwAMwj-PFIHBx7JR8AHNOa3_494",
+        accessToken: authData.customerToken,
       },
     });
     cy.get('button[type="submit"]').click();
