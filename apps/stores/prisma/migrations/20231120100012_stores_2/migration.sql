504dc4441589f68db4364ac5dd09ced50ab5c0a6
Ala Eddin A. Aldoukhi <alaad03@icloud.com>
2024-05-12 10:48:18 +0300

    removed migrations

--- apps/stores/prisma/migrations/20231120100012_stores_2/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[storeId]` on the table `BusinessStore` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "BusinessStore_storeId_key" ON "BusinessStore"("storeId");
