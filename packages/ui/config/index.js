764ccc1b3f2f18c552965ae3aa089483573c9ed4
Albaraa Hasan <albaraa.hassan@zid.sa>
2024-05-30 02:20:37 +0300

    js support

---
+++ packages/ui/config/index.js
@@ -0,0 +1,87 @@
+export const config = {
+  platformName: "Cars4Sale",
+  platformDescription: "Cars4Sale is a platform for buying and selling cars",
+  version: "0.1.1",
+  about: {
+    name: "about",
+  },
+  contact: {
+    name: "contact",
+  },
+  footer: {
+    sections: [
+      {
+        title: "Legal",
+        items: [
+          {
+            title: "Terms & Conditions",
+            path: "#",
+          },
+          {
+            title: "License",
+            path: "#",
+          },
+        ],
+      },
+      {
+        title: "Social",
+        items: [
+          {
+            title: "Instagram",
+            path: "#",
+          },
+          {
+            title: "LinkedIn",
+            path: "#",
+          },
+        ],
+      },
+    ],
+  },
+  store: {
+    name: "garage",
+    plural: "garages",
+    individual: {
+      name: "personal garage",
+      description:
+        "Personal Garages are designed to meet personal offerings of cars.",
+    },
+    business: {
+      name: "car dealership",
+      description:
+        "Car Dealerships are designed to meet branding and professional requirements, as well as the needs of establishments with a variety of cars.",
+    },
+    description: "Garages are designed to meet the needs of car sellers. They can be used to showcase cars, manage orders, and more. Join us and create your own garage today!",
+  },
+  catalog: {
+    name: "catalog",
+    item: {
+      name: "car",
+      plural: "cars",
+    },
+    category: {
+      name: "category",
+      plural: "categories",
+    },
+    option: {
+      name: "option",
+      plural: "options",
+    },
+    optionGroup: {
+      name: "option group",
+      plural: "option groups",
+      example:
+        "For example, 'Size' is an option group while 'Small', 'Medium', and 'Large' are options.",
+    },
+    variant: {
+      name: "variant",
+    },
+  },
+  cart: {
+    name: "cart",
+  },
+  order: {
+    name: "order",
+    plural: "orders",
+  },
+};
